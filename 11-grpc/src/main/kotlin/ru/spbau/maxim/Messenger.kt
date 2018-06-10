f65547511828033cfa2b0d068ca5de5010c0722a
maxkvant <max.kvant@gmail.com>
2018-06-10 20:40:46 +0300

    logging

--- 11-grpc/src/main/kotlin/ru/spbau/maxim/Messenger.kt
+++ 11-grpc/src/main/kotlin/ru/spbau/maxim/Messenger.kt
@@ -6,12 +6,11 @@ import io.grpc.stub.StreamObserver
 import com.google.protobuf.Timestamp
 import java.util.concurrent.TimeUnit
 import java.time.Instant
-
+import java.util.logging.Logger
 
 
 class Messenger(val userName: String, port: Int, peerAddress: PeerAddress, val onMessage: (Protocol.Message) -> Unit) {
-    data class PeerAddress(val url: String, val port: Int)
-
+    private val logger = Logger.getLogger(Messenger::class.java.name)
     private val server = ServerBuilder.forPort(port)
             .addService(MessengerService())
             .build()
@@ -20,27 +19,47 @@ class Messenger(val userName: String, port: Int, peerAddress: PeerAddress, val o
     private val channel = ManagedChannelBuilder.forAddress(peerAddress.url, peerAddress.port).usePlaintext().build()
     private val stub = MessengerServiceGrpc.newBlockingStub(channel)
 
-    fun sendMessage(text: String) {
+    init {
+        Runtime.getRuntime().addShutdownHook(Thread { server.shutdown() })
+        logger.info("Messenger is initialized")
+    }
+
+    fun sendMessage(text: String): Boolean {
         val time = Instant.now()
         val timestamp = Timestamp.newBuilder().setSeconds(time.epochSecond).build()
 
         val message = Protocol.Message.newBuilder().setUserName(userName).setText(text).setDate(timestamp).build()
         try {
             stub.sendMessage(message)
+            logger.info("message sent")
         } catch (e: Exception) {
-            //TODO log
+            logger.warning("error during sending message")
+            return false
+        }
+        return true
+    }
+
+    fun shutdown() {
+        try {
+            logger.info("shutting down messenger")
+            server.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+            channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+        } catch (e: Exception) {
+            logger.warning("error during shutdown: ${e.message}")
         }
     }
 
+    data class PeerAddress(val url: String, val port: Int)
+
     private inner class MessengerService : MessengerServiceGrpc.MessengerServiceImplBase() {
         override fun sendMessage(request: Protocol.Message, responseObserver: StreamObserver<Protocol.EmptyResult>) {
-            //TODO log
+            logger.entering(MessengerService::class.java.simpleName, "received message")
 
             onMessage(request)
             responseObserver.onNext(Protocol.EmptyResult.newBuilder().build())
             responseObserver.onCompleted()
 
-            //TODO log
+            logger.exiting(MessengerService::class.java.simpleName, "received message")
         }
     }
 }
--- 11-grpc/src/main/kotlin/ru/spbau/maxim/main.kt
+++ 11-grpc/src/main/kotlin/ru/spbau/maxim/main.kt
@@ -1,6 +1,7 @@
 package ru.spbau.maxim
 
 import io.grpc.ServerBuilder
+import java.lang.Thread.sleep
 import java.sql.Date
 import java.text.SimpleDateFormat
 
@@ -8,14 +9,12 @@ import java.text.SimpleDateFormat
 
 fun main(args2: Array<String>) {
     val args = arrayOf("maxim", "12345", "localhost", "23456")
-
-    require(args.size == 4) {
-        """
-        Usage:
-        user_name port peer_url peer_port
-        """
+    if(args.size != 4) {
+        val help =  """|Usage:
+                       |user_name port peer_url peer_port
+                       |""".trimMargin()
+        print(help)
     }
-
 
     val userName = args[0]
     val port = args[1].toInt()
@@ -24,29 +23,30 @@ fun main(args2: Array<String>) {
     val peerPort = args[3].toInt()
     val peerAddress = Messenger.PeerAddress(peerUrl, peerPort)
 
-    val messenger = Messenger(userName, port, peerAddress, { message ->
-        println("from: ${message.userName}, date: ${message.date}")
-        println("\"\"\"")
-        println(message.text)
-        println("\"\"\"")
-        println()
-    })
-
-    val messenger2 = Messenger("messenger2", peerPort, Messenger.PeerAddress("localhost", port), { message ->
+    val onMessage = { message: Protocol.Message ->
         val date = Date(message.date.seconds * 1000)
         val dateformat = SimpleDateFormat("HH:mm:ss MMM dd, yyy")
         println("from: ${message.userName}, date: ${dateformat.format(date)}")
+        println("\"\"\"")
         println(message.text)
+        println("\"\"\"")
         println()
-    })
+    }
+
+    val messenger = Messenger(userName, port, peerAddress, onMessage)
+    val messenger2 = Messenger("messenger2", peerPort, Messenger.PeerAddress("localhost", port), onMessage)
     messenger2.sendMessage("прив")
 
     while (true) {
         val line: String? = readLine()
-        if (line == null) {
+        if (line == null || line == "exit") {
             break
-        } else {
-            messenger.sendMessage(line)
+        }
+        val messageSent = messenger.sendMessage(line)
+        if (!messageSent) {
+            println("Couldn't send message")
         }
     }
+
+    messenger.shutdown()
 }
